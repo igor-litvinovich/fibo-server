b0f3adaa0552f05b247a9cf6040e6ea886f515fc
Ihar Litvinovich <ihar.litvinovich@itechart-group.com>
2019-06-11 15:38:46 +0300

    docker run test#1

--- devops/run_me.sh
+++ devops/run_me.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
-docker run --rm \
-  --env 'NODE_ENV=development' \
-  --env 'DB_HOST=host.docker.internal' \
-   -p 8080:8080 \
-   -it igorlitv/fibo-server:latest
+docker run --rm --env 'NODE_ENV=development' -p 8080:8080 -it igorlitv/fibo-server:latest
