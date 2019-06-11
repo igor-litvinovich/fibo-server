03cf58adf0fcace6d8b36c2fb5ba4e9a82954717
Ihar Litvinovich <ihar.litvinovich@itechart-group.com>
2019-06-11 17:13:55 +0300

    deploy test#13

--- devops/run_me.sh
+++ devops/run_me.sh
@@ -1,1 +1,1 @@
-sudo docker pull igorlitv/fibo-server:latest && sudo docker run -d --rm --env 'NODE_ENV=development' -p 8080:8080 igorlitv/fibo-server:latest
+"sudo docker pull igorlitv/fibo-server:latest; sudo docker run -d --rm --env 'NODE_ENV=development' -p 8080:8080 igorlitv/fibo-server:latest"
