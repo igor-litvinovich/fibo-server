804600ce09d56ee47d7aa28682a2df6b65cfee0c
Ihar Litvinovich <ihar.litvinovich@itechart-group.com>
2019-06-11 17:02:52 +0300

    deploy test#11

--- devops/run_me.sh
+++ devops/run_me.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
-docker run --rm --env 'NODE_ENV=development' -p 8080:8080 -it igorlitv/fibo-server:latest
+sudo docker pull igorlitv/fibo-server:latest && sudo docker run -d --rm --env 'NODE_ENV=development' -p 8080:8080 igorlitv/fibo-server:latest
