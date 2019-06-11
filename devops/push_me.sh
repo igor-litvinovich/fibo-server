1abf277045d20f99bfdb8d79e60e89b1b3c59680
Ihar Litvinovich <ihar.litvinovich@itechart-group.com>
2019-06-11 15:22:03 +0300

    docker push test#2

--- devops/push_me.sh
+++ devops/push_me.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin "https://cloud.docker.com/repository/docker/igorlitv/fibo-server"
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
 docker push igorlitv/fibo-server:latest
