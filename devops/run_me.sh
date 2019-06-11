e4894e1d745368e3a031c8ec283efe24981a64ab
Ihar Litvinovich <ihar.litvinovich@itechart-group.com>
2019-06-11 17:57:21 +0300

    deploy test#21

--- devops/run_me.sh
+++ devops/run_me.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-sshpass -p$SSH_PASS ssh -o stricthostkeychecking=no -f -tt $SSH_USER@$SSH_HOST "sudo docker pull igorlitv/fibo-server:latest; sudo docker run -d --rm --env 'NODE_ENV=development' -p 8080:8080 igorlitv/fibo-server:latest"
+sshpass -p$SSH_PASS ssh -o stricthostkeychecking=no -f -tt $SSH_USER@$SSH_HOST "sudo docker pull igorlitv/fibo-server:latest"
+echo "waiting 30 seconds..."
+sshpass -p$SSH_PASS ssh -o stricthostkeychecking=no -f -tt $SSH_USER@$SSH_HOST "sudo docker run -d --rm --env 'NODE_ENV=development' -p 8080:8080 igorlitv/fibo-server:latest"
