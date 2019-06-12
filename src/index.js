7bfce1340088df0e0901230626f5ab0c93c90639
Igor Litvinovich <ihar.litvinovich@gmail.com>
2019-06-12 18:21:31 +0300

    test deploy #60

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ app.get('/fibo', async (req, res) => {
         result: number,
         createdDate: new Date()
     });*/
-    res.json({ number, version: '1.0.10' });
+    res.json({ number, version: '1.0.11' });
 });
 
 const port = config.get('server.port');
