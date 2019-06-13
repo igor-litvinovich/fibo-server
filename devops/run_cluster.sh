0bb0f17de103e9e79c6297385bd8d43a2a71b69a
Igor Litvinovich <ihar.litvinovich@gmail.com>
2019-06-13 12:19:53 +0300

    test deploy #64

--- devops/run_cluster.sh
+++ devops/run_cluster.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo docker pull igorlitv/fibo-server:latest > /dev/null 2>&1
-sudo docker stack deploy -c devops/docker-compose.yml server_name > /dev/null 2>&1
+sudo docker pull igorlitv/fibo-server:latest
+sudo docker stack deploy -c devops/docker-compose.yml server_name
