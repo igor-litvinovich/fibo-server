0e657f4501bd4de35cbefea44bd39b11785117b0
Igor Litvinovich <ihar.litvinovich@gmail.com>
2019-06-13 14:41:44 +0300

    test deploy #74

--- devops/run_cluster.sh
+++ devops/run_cluster.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-sudo docker pull igorlitv/fibo-server:latest; sudo docker service rm server_name_fibo-server; sudo docker stack deploy -c devops/docker-compose.yml server_name
+sudo docker pull igorlitv/fibo-server:latest; \
+sudo docker rmi $(sudo docker images -q --filter "dangling=true"); \
+sudo docker service rm server_name_fibo-server; \
+sudo docker stack deploy -c devops/docker-compose.yml server_name
