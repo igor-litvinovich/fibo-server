91cfa5b9f3322f1c2f3ef94143c295109988fb04
Igor Litvinovich <ihar.litvinovich@gmail.com>
2019-06-17 14:56:06 +0300

    test heroku release #6

--- devops/heroku/release_heroku.sh
+++ devops/heroku/release_heroku.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "Running release"
+echo "Running release $TRAVIS_TAG"
 
 docker login --username="$HEROKU_USERNAME" --password=$(heroku auth:token) registry.heroku.com && \
 docker tag igorlitv/fibo-server:"$TRAVIS_TAG" registry.heroku.com/"$HEROKU_APP_NAME"/web && \
