263da956d02305e51d099fcd2cb8131a1a2a7b3b
Igor Litvinovich <ihar.litvinovich@gmail.com>
2019-12-07 16:32:41 +0300

    added keys

--- devops/digital-ocean/before_deploy.sh
+++ devops/digital-ocean/before_deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 openssl aes-256-cbc -K $encrypted_bf7b528e85e7_key -iv $encrypted_bf7b528e85e7_iv -in travis_deploy_key.pem.enc -out travis_deploy_key.pem -d
-chmod 600 ./travis_deploy_key
-ssh-add ./travis_deploy_key
+chmod 600 travis_deploy_key
+ssh-add travis_deploy_key
 
 scp -i travis_deploy_key -o StrictHostKeyChecking=no -rp ./devops $SSH_USER@$SSH_HOST:/$SSH_USER
 ssh -i travis_deploy_key -o StrictHostKeyChecking=no -tt  $SSH_USER@$SSH_HOST "chmod +rwx ./devops/run_cluster.sh && 
