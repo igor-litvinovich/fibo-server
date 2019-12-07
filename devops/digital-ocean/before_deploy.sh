5b8548d1a23cc878a091a1766ffb1e623dfc1270
Igor Litvinovich <ihar.litvinovich@gmail.com>
2019-12-07 16:40:11 +0300

    added keys

--- devops/digital-ocean/before_deploy.sh
+++ devops/digital-ocean/before_deploy.sh
@@ -3,6 +3,6 @@ openssl aes-256-cbc -K $encrypted_bf7b528e85e7_key -iv $encrypted_bf7b528e85e7_i
 chmod 600 ./travis_deploy_key.pem
 ssh-add ./travis_deploy_key.pem
 
-scp -i travis_deploy_key -o StrictHostKeyChecking=no -rp ./devops $SSH_USER@$SSH_HOST:/$SSH_USER
-ssh -i travis_deploy_key -o StrictHostKeyChecking=no -tt  $SSH_USER@$SSH_HOST "chmod +rwx ./devops/run_cluster.sh && 
+scp -i ./travis_deploy_key.pem -o StrictHostKeyChecking=no -rp ./devops $SSH_USER@$SSH_HOST:/$SSH_USER
+ssh -i ./travis_deploy_key.pem -o StrictHostKeyChecking=no -tt  $SSH_USER@$SSH_HOST "chmod +rwx ./devops/run_cluster.sh && 
                     echo \"$DOCKER_PASSWORD\" | docker login -u \"$DOCKER_USERNAME\" --password-stdin"
