5869dbe52d20ecf429cda68fbf7b97cdf1856f58
Igor Litvinovich <ihar.litvinovich@gmail.com>
2019-12-08 11:46:38 +0300

    updated configs

--- devops/digital-ocean/before_deploy.sh
+++ devops/digital-ocean/before_deploy.sh
@@ -4,5 +4,5 @@ chmod 600 ./travis_key
 ssh-add ./travis_key
 
 scp -i travis_key -o StrictHostKeyChecking=no -rp ./devops $SSH_USER@$SSH_HOST:/$SSH_USER
-ssh -i travis_key -o StrictHostKeyChecking=no -tt  $SSH_USER@$SSH_HOST "chmod +rwx ./run_cluster.sh &&
+ssh -i travis_key -o StrictHostKeyChecking=no -tt  $SSH_USER@$SSH_HOST "chmod +rwx ./devops/run_cluster.sh &&
                     echo \"$DOCKER_PASSWORD\" | docker login -u \"$DOCKER_USERNAME\" --password-stdin"
