f85379df59ea444d6b488fcec8bb357d7bacaa40
Igor Litvinovich <ihar.litvinovich@gmail.com>
2020-02-16 16:16:41 +0300

    Update release_heroku.sh

--- devops/heroku/release_heroku.sh
+++ devops/heroku/release_heroku.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Running release $TAG"
 wget -qO- https://toolbelt.heroku.com/install.sh | sh && \
-docker login --username="$HEROKU_USERNAME" --password="$(heroku auth:token)" registry.heroku.com && \
+docker login --username="$HEROKU_USERNAME" --password="$HEROKU_API_KEY" registry.heroku.com && \
 docker tag igorlitv/fibo-server:"$TAG" registry.heroku.com/"$HEROKU_APP_NAME"/web && \
 docker push registry.heroku.com/"$HEROKU_APP_NAME"/web && \
 heroku container:release web --app "$HEROKU_APP_NAME"
