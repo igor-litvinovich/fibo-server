9a196eb14b0c09af3b77d9e4eb7f895bd15a03b8
Igor Litvinovich <ihar.litvinovich@gmail.com>
2020-02-16 14:36:07 +0300

    Updated Ci config #7

--- devops/heroku/release_heroku.sh
+++ devops/heroku/release_heroku.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-echo "Running release $TRAVIS_TAG"
+echo "Running release $TAG"
 wget -qO- https://toolbelt.heroku.com/install.sh | sh && \
 docker login --username="$HEROKU_USERNAME" --password=$(heroku auth:token) registry.heroku.com && \
-docker tag igorlitv/fibo-server:"$TRAVIS_TAG" registry.heroku.com/"$HEROKU_APP_NAME"/web && \
+docker tag igorlitv/fibo-server:"$TAG" registry.heroku.com/"$HEROKU_APP_NAME"/web && \
 docker push registry.heroku.com/"$HEROKU_APP_NAME"/web && \
 heroku container:release web --app "$HEROKU_APP_NAME"
