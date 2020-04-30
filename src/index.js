e30537221add16a3bb5e9ffac5dbf365da916428
Igor Litvinovich <ihar.litvinovich@gmail.com>
2020-04-30 11:45:10 +0300

    Updated version to 2.1.9

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ app.get('/fibo', async(req, res) => {
         })
     }
     const number = fibo(Number.parseInt(after));
-    res.json({ number, version: '2.1.8' });
+    res.json({ number, version: '2.1.9' });
 });
 
 const port = config.get('server.port');
